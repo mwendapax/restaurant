a3accb3149ee671be6b41f8e7785af7dd70ad68a
mwendapax <160007831+mwendapax@users.noreply.github.com>
2024-09-27 16:39:05 +0300

    Feat: Add code fore home dynamically with JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './styles.css';
+import {home as homePage} from './home.js';
+
+
+(function addToDom () {
+    const main = document.querySelector('#content');
+    const homeButton = document.querySelector('.home-btn');
+
+    homeButton.addEventListener('click', (e) => {
+        main.appendChild(homePage.floatingCard);
+        main.appendChild(homePage.homeContent);
+        e.target.style.color = 'white';
+    });
+
+    homeButton.click();
+})();
