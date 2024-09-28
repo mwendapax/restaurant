9dec957b2bfd8811ff0400031eece78d30ed260d
mwendapax <160007831+mwendapax@users.noreply.github.com>
2024-09-28 16:06:52 +0300

    Feat: Add markup for menu dynamically with JS

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import './styles.css';
 import {home as homePage} from './home.js';
+import {menu as menuPage} from './menu.js';
 
 
 (function addToDom () {
     const main = document.querySelector('#content');
     const homeButton = document.querySelector('.home-btn');
+    const menuButton = document.querySelector('.menu-btn');
 
     homeButton.addEventListener('click', (e) => {
+        main.innerHTML = '';
         main.appendChild(homePage.floatingCard);
         main.appendChild(homePage.homeContent);
         e.target.style.color = 'white';
+        menuButton.style.color = 'coral';
     });
+
+    menuButton.addEventListener('click', (e) => {
+        main.innerHTML = '';
+        main.appendChild(menuPage.menuContent);
+        e.target.style.color = 'white';
+        homeButton.style.color = 'coral';
+    });
+
+
+
+
 
     homeButton.click();
 })();
