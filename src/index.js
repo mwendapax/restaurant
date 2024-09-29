2ebdd06c2bcfb85225b3a9c556e5871a66274b97
mwendapax <160007831+mwendapax@users.noreply.github.com>
2024-09-29 16:40:37 +0300

    Feat: Add contact page markup dynamically with JS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './styles.css';
 import {home as homePage} from './home.js';
 import {menu as menuPage} from './menu.js';
+import {contact as contactPage} from './contact.js';
 
 
 (function addToDom () {
     const main = document.querySelector('#content');
     const homeButton = document.querySelector('.home-btn');
     const menuButton = document.querySelector('.menu-btn');
+    const contactButton = document.querySelector('.contact-btn');
 
     homeButton.addEventListener('click', (e) => {
         main.innerHTML = '';
@@ -14,6 +16,7 @@ import {menu as menuPage} from './menu.js';
         main.appendChild(homePage.homeContent);
         e.target.style.color = 'white';
         menuButton.style.color = 'coral';
+        contactButton.style.color = 'coral';
     });
 
     menuButton.addEventListener('click', (e) => {
@@ -21,9 +24,16 @@ import {menu as menuPage} from './menu.js';
         main.appendChild(menuPage.menuContent);
         e.target.style.color = 'white';
         homeButton.style.color = 'coral';
+        contactButton.style.color = 'coral';
     });
 
-
+    contactButton.addEventListener('click', (e) => {
+        main.innerHTML = '';
+        main.appendChild(contactPage.contactContainer);
+        e.target.style.color = 'white';
+        homeButton.style.color = 'coral';
+        menuButton.style.color = 'coral';
+    })
 
 
 
